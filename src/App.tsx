f123f3a28dfaae90f72f11cb6c069d0663252b88
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-05-23 14:29:10 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
