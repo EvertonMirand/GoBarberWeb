bc27f9320988702f4e0670aa4cd0bc590b5a143e
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-06-02 20:33:14 -0300

    Validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { Form } from '@unform/web';
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
@@ -11,9 +12,22 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object): void => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório.'),
+        email: Yup.string()
+          .required('E-mail orbigatório.')
+          .email('Digite um e-mail válido.'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos.'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
